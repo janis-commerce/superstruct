dcdafeec095a1bfb65e755101ec8a3c1f7d3c1e4
alejandro <ale.g.acosta08@gmail.com>
2019-07-05 14:55:55 -0300

    init package superstruct

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { Superstruct } = require('./lib');
+
+module.exports = Superstruct;
---
+++ tests/superstruct-test.js
@@ -0,0 +1,14 @@
+/* eslint-disable  */
+'use strict';
+
+// const assert = require('assert');
+
+// const sandbox = require('sinon').createSandbox();
+
+// const { Superstruct } = require('./../lib');
+
+describe('Superstruct', () => {
+
+	// your tests here...
+
+});
